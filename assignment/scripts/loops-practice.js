a35a1b9e912c3e12e469a66ff85222eded355842
Kas Eam <KassandraEam@gmail.com>
2022-06-24 10:46:01 -0500

    Completed Problems 2.a - 3.a

--- assignment/scripts/loops-practice.js
+++ assignment/scripts/loops-practice.js
@@ -50,16 +50,40 @@ let stars = ['Polaris', 'Gacrux', 'Formalhaut', 'Rigel', 'Deneb']
 
 // 2.a. TODO: Write a for of loop to console.log each star in the 'stars' array
 console.log('Some stars:');
-
+for (let starLoop of stars){
+  console.log(starLoop);
+}
 
 // 3. While loops  
 console.log('---- 3. While loop ----');
 
 // 3.a. TODO: Write a while loop to console.log each star in the 'stars' array
 console.log('Some stars using while:');
+let i = 0;
+while (i<stars.length){
+  console.log(stars[i])
+  i++;
+};
+
+// initialization
+// while (condition) {
+//   // code block to be executed
+//   // iteration
+// }
+ 
+// let i = 0;
+// while (i < 5) {        
+//   console.log(i);
+//   i++;
+// }
 
 // 3.b. TODO: Write a while loop to console.log the numbers from 0 to 5
 console.log('count from 0 to 5');
-
+// let i = 0; don't need to reinitialize this again because it's already been done above?
+while (i<6){
+  let i = 0;
+  console.log(i);
+  i++;
+};
 // 3.c. TODO: Write a while loop to console.log the numbers from 10 to 5
 console.log('count backwards from 10 to 5');
